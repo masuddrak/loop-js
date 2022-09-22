32affbdfd825ea1d0c232e80aae5f469e1fc3153
masud rana <masudranaabc@gmail.com>
2022-09-22 11:35:14 +0600

    array added while loop

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 // while loop 
-let a=1;
-while(a<=10){
-    console.log(a)
-    a++
+// let a=1;
+// while(a<=10){
+//     console.log(a)
+//     a++
+// }
+let numbers=[10,11,12,14,15]
+let i=0;
+while(i<numbers.length){
+    console.log(numbers[i])
+    i++
 }
