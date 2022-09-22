b444ab7c327656572617eb4abcffe9c639b8f97c
masud rana <masudranaabc@gmail.com>
2022-09-22 22:42:29 +0600

    added switch case

---
+++ switchCase.js
@@ -0,0 +1,14 @@
+let num=20;
+switch(num){
+    case 4:
+        console.log('the number is 4')
+        break;
+    case 2:
+        console.log('the number is 2')
+        break;
+    case 20:
+        console.log('the number is 20')
+        break;
+        default:
+            console.log('worg your input typr')
+}
