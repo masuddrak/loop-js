9344ff93e7d7a46c550b6e7a72e05e88851790fe
masud rana <masudranaabc@gmail.com>
2022-09-22 22:16:08 +0600

    vjange object valu

--- object.js
+++ object.js
@@ -1,6 +1,14 @@
-let student={
-    name:'Masud',
-    roll:22,
-    id:265454
-}
-console.log(student)
+// let student={
+//     name:'Masud',
+//     roll:22,
+//     id:265454
+// }
+// console.log(student);
+let pc={
+    name:"indig pc",
+    price:500,
+    color:'Yello'
+};
+console.log(pc)
+pc.price=2000
+console.log(pc)
