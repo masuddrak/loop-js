3b82f4d82218735219cd3f22e8a469db16692f17
masud rana <masudranaabc@gmail.com>
2022-09-22 23:19:28 +0600

    for loop array break addded

--- break-continue.js
+++ break-continue.js
@@ -1,7 +1,7 @@
 let numbers=[10,12,15,1,8,8,9,6,77]
 for(i=0;i<numbers.length;i++){
-    if(numbers[i]==10){
-        continue
+    if(numbers[i]<8){
+        break
     }
     console.log(numbers[i])
 }
