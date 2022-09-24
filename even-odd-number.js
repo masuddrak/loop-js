c61e09e7ae9ffe551e5f0dac3a0e5951da63df51
masud rana <masudranaabc@gmail.com>
2022-09-24 13:51:50 +0600

    added event or odd number

---
+++ even-odd-number.js
@@ -0,0 +1,12 @@
+
+function eventOdd(number) {
+    if (number % 2 == 0) {
+        // console.log('this number event')
+        return true
+    } else {
+        // console.log('this number event')
+        return false
+    }
+}
+let checking =eventOdd(221)
+console.log(checking)
