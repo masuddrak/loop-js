5cf1bbf39bca423fa922a2c1dcb3dd0af4860707
masud rana <masudranaabc@gmail.com>
2022-09-24 23:39:52 +0600

    aded math.max

---
+++ max.js
@@ -0,0 +1,8 @@
+function bigestNumber(frist, secont, third) {
+    let largest = Math.max(frist, secont, third)
+
+    return largest;
+}
+
+const result = bigestNumber(24, 48, 12)
+console.log(result)
